60674f6dada17ed2f2022d54ebfe24be4ca02441
RealVetchez <77519115+RealVetchez@users.noreply.github.com>
2023-01-11 23:43:19 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let flag = args[1].clone();
+    let threads = args[2].clone();
+    let ipaddr = args[3].clone();
+
+}
